03ee8937d5655fe13a8b4ff4e2e4be7e19e424c1
Fabian Gut <fabian.gut@swissinfo.ch>
2017-12-12 13:17:36 +0100

    Improved type inference

--- src/ch/inftec/typeinference/TypeInference.java
+++ src/ch/inftec/typeinference/TypeInference.java
@@ -1,7 +1,13 @@
 package ch.inftec.typeinference;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TypeInference {
   public static void foo() {
-
+    List<String> list = new ArrayList<>();
+    list.add("A");
+    list.addAll(Arrays.asList("a", "b"));
   }
 }
