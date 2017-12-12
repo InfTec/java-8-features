e51255d8694a242c553b8c0a9a02af00c5c38810
Fabian Gut <fabian.gut@swissinfo.ch>
2017-12-12 13:18:20 +0100

    Streams

--- src/ch/inftec/streams/Streams.java
+++ src/ch/inftec/streams/Streams.java
@@ -2,10 +2,26 @@ package ch.inftec.streams;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Streams {
   public static void main(String[] args) {
     List<Integer> list = getList(100);
+
+    list.stream().forEach(System.out::println);
+
+    Optional<Integer> optionalSum = list.stream().reduce(Integer::sum);
+    if(optionalSum.isPresent()) {
+      Integer integer = optionalSum.get();
+      System.out.println("\nsum: " + integer);
+    }
+
+//    list.stream().filter(i -> i > 90).forEach(System.out::println);
+    list.parallelStream().filter(i -> i > 90).forEach(System.out::println);
+    list.stream().filter(i -> i > 90).parallel().filter(i -> i % 2 == 0).parallel().forEach(System.out::println);
+
+    long count = list.stream().count();
+    System.out.println("size of list: " + count);
   }
 
   private static List<Integer> getList(int size) {
